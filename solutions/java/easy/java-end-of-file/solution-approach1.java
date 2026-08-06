e367d812ac6dbe92d4af2785d88082ddb507f193
Shreyash K Ambule <shreyashambule@gmail.com>
2026-08-07 01:08:58 +0530

    ✅ Java End-of-file [java] — approach1

---
+++ solutions/java/easy/java-end-of-file/solution-approach1.java
@@ -0,0 +1,33 @@
+// ──────────────────────────────────────────────────
+// Link        https://www.hackerrank.com/challenges/java-end-of-file/problem?isFullScreen=true
+// Problem     Java End-of-file
+// Difficulty  Easy
+// Subdomain   Introduction
+// Platform    HackerRank
+// Language    java
+// Status      Accepted
+// Submitted   2026-08-07, 01:08 a.m.
+// ──────────────────────────────────────────────────
+
+import java.io.*;
+import java.util.*;
+import java.text.*;
+import java.math.*;
+import java.util.regex.*;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        /* Enter your code here. Read input from STDIN. Print output to STDOUT. Your class should be named Solution. */
+        Scanner sc = new Scanner(System.in);
+        int linen=1;
+        
+        while(sc.hasNext()){
+            String line= sc.nextLine();
+            System.out.println(linen+" "+line);
+            linen++;
+            
+        }
+        
+    }
+}
