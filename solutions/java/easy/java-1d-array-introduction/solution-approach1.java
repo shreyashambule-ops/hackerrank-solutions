021e7eae13377d143c04c3800e3fa499e9c87967
Shreyash K Ambule <shreyashambule@gmail.com>
2026-09-07 11:46:08 +0530

    ✅ Java 1D Array [java] — approach1

---
+++ solutions/java/easy/java-1d-array-introduction/solution-approach1.java
@@ -0,0 +1,19 @@
+// ──────────────────────────────────────────────────
+// Link        https://www.hackerrank.com/challenges/java-1d-array-introduction/problem?isFullScreen=true
+// Problem     Java 1D Array
+// Difficulty  Easy
+// Subdomain   Data Structures
+// Platform    HackerRank
+// Language    java
+// Status      Accepted
+// Submitted   2026-09-07, 11:46 a.m.
+// ──────────────────────────────────────────────────
+
+
+        int[] a=new int[n];
+        for(int i=0;i<n;i++){
+            a[i]=scan.nextInt();
+        }
+        
+        
+
