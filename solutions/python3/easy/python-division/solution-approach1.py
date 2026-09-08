0d7e8b7a69918e15263694957c375ffa00d4eaa9
Shreyash K Ambule <shreyashambule@gmail.com>
2026-09-08 23:01:52 +0530

    ✅ Python: Division [python3] — approach1

---
+++ solutions/python3/easy/python-division/solution-approach1.py
@@ -0,0 +1,17 @@
+# ──────────────────────────────────────────────────
+# Link        https://www.hackerrank.com/challenges/python-division/problem?isFullScreen=true
+# Problem     Python: Division
+# Difficulty  Easy
+# Subdomain   Introduction
+# Platform    HackerRank
+# Language    python3
+# Status      Accepted
+# Submitted   2026-09-08, 11:01 p.m.
+# ──────────────────────────────────────────────────
+
+if __name__ == '__main__':
+    a = int(input())
+    b = int(input())
+    
+    print(int(a/b))
+    print(a/b)
