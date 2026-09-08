6ae8d30b7522c8de568052ae93409ff8e436eb33
Shreyash K Ambule <shreyashambule@gmail.com>
2026-09-08 22:59:34 +0530

    ✅ Arithmetic Operators [python3] — approach1

---
+++ solutions/python3/easy/python-arithmetic-operators/solution-approach1.py
@@ -0,0 +1,18 @@
+# ──────────────────────────────────────────────────
+# Link        https://www.hackerrank.com/challenges/python-arithmetic-operators/problem?isFullScreen=true
+# Problem     Arithmetic Operators
+# Difficulty  Easy
+# Subdomain   Introduction
+# Platform    HackerRank
+# Language    python3
+# Status      Accepted
+# Submitted   2026-09-08, 10:59 p.m.
+# ──────────────────────────────────────────────────
+
+if __name__ == '__main__':
+    a = int(input())
+    b = int(input())
+    
+    print(a+b)
+    print(a-b)
+    print(a*b)
