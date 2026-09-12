9cf33e1bc6b67e909b0e71be15c10ea8b4eb8405
Shreyash K Ambule <shreyashambule@gmail.com>
2026-09-12 16:21:53 +0530

    ✅ Print Function [python3] — approach1

---
+++ solutions/python3/easy/python-print/solution-approach1.py
@@ -0,0 +1,15 @@
+# ──────────────────────────────────────────────────
+# Link        https://www.hackerrank.com/challenges/python-print/problem?isFullScreen=true
+# Problem     Print Function
+# Difficulty  Easy
+# Subdomain   Introduction
+# Platform    HackerRank
+# Language    python3
+# Status      Accepted
+# Submitted   2026-09-12, 04:21 p.m.
+# ──────────────────────────────────────────────────
+
+if __name__ == '__main__':
+    n = int(input())
+    for i in range(1,n+1):
+        print(i, end="")
